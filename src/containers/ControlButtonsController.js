ed65661717da5df76cccb11629f55e5fc4c45326
Michael Orlov <MiklerGM@gmail.com>
2017-01-27 22:03:54 +0300

    Extracting logic from presentational container

---
+++ src/containers/ControlButtonsController.js
@@ -0,0 +1,45 @@
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import * as timelineActions from '../reducers/timeline';
+import ControlButtons from '../components/ControlButtons';
+
+class ControlButtonsController extends Component {
+  togglePlay = () => {
+    if (this.props.timeline.intervalId) {
+      clearInterval(this.props.timeline.intervalId);
+      this.props.timelineAction.saveIntervalId(0);
+    } else {
+      this.props.timelineAction.saveIntervalId(setInterval(this.props.timelineAction.nextYear,
+         this.props.timeline.interval));
+    }
+  }
+
+  render() {
+    return (<ControlButtons
+      timeline={this.props.timeline}
+      reestYear={this.props.timelineAction.resetYear}
+      togglePlay={this.togglePlay}
+    />
+    );
+  }
+}
+ControlButtonsController.propTypes = {
+  timeline: PropTypes.object.isRequired,
+  timelineAction: PropTypes.object.isRequired
+};
+
+function mapStateToProps(state) {
+  return { timeline: {
+    now: state.timeline.now,
+    interval: state.timeline.interval,
+    intervalId: state.timeline.intervalId
+  }
+  };
+}
+function mapDispatchToProps(dispatch) {
+  return {
+    timelineAction: bindActionCreators(timelineActions, dispatch),
+  };
+}
+export default connect(mapStateToProps, mapDispatchToProps)(ControlButtonsController);
--- src/containers/Header.js
+++ src/containers/Header.js
@@ -1,43 +1,12 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
-import * as timelineActions from '../reducers/timeline';
-import ControlButtons from '../components/ControlButtons';
+import ControlButtonsController from './ControlButtonsController';
 
-function togglePlay() {
-  if (this.timeline.intervalId) {
-    clearInterval(this.timeline.intervalId);
-    this.timelineAction.saveIntervalId(0);
-  } else {
-    this.timelineAction.saveIntervalId(setInterval(this.timelineAction.nextYear,
-       this.timeline.interval));
-  }
-}
-
-const Header = ({ timeline, timelineAction }) => (
+const Header = () => (
   <div className='header'>
     <span>
       Map (Thanks, Captain)
+      <ControlButtonsController />
     </span>
-    <ControlButtons
-      timeline={timeline}
-      reestYear={timelineAction.resetYear}
-      togglePlay={togglePlay.bind({ timeline, timelineAction })}
-    />
   </div>
 );
-function mapStateToProps(state) {
-  return { timeline: {
-    now: state.timeline.now,
-    limit: state.timeline.max - state.timeline.now,
-    interval: state.timeline.interval,
-    intervalId: state.timeline.intervalId
-  }
-  };
-}
-function mapDispatchToProps(dispatch) {
-  return {
-    timelineAction: bindActionCreators(timelineActions, dispatch),
-  };
-}
-export default connect(mapStateToProps, mapDispatchToProps)(Header);
+export default Header;
