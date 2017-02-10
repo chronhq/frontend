ae774edba94043aabd1b18d2b68a3e854da89295
Michael Orlov <MiklerGM@gmail.com>
2017-02-10 17:28:04 +0300

    Added refux-thunk middleware for no purpose

--- src/store/configureStore.development.js
+++ src/store/configureStore.development.js
@@ -1,11 +1,13 @@
 import { applyMiddleware, createStore, compose } from 'redux';
 import { persistState } from 'redux-devtools';  // eslint-disable-line
+import thunk from 'redux-thunk';
 import promiseMiddleware from 'redux-promise-middleware';
 import rootReducer from '../reducers';
 import DevTools from '../DevTools';
 
+const middleware = [promiseMiddleware(), thunk];
 const enhancer = compose(
-  applyMiddleware(promiseMiddleware()),
+  applyMiddleware(...middleware),
   DevTools.instrument(),
   persistState(
     window.location.href.match(
--- src/store/configureStore.production.js
+++ src/store/configureStore.production.js
@@ -1,8 +1,9 @@
 import { applyMiddleware, createStore } from 'redux';
 import promiseMiddleware from 'redux-promise-middleware';
+import thunkMiddleware from 'redux-thunk';
 import rootReducer from '../reducers';
 
-const enhancer = applyMiddleware(promiseMiddleware());
+const enhancer = applyMiddleware(promiseMiddleware(), thunkMiddleware());
 
 export default function configureStore(initialState) {
   return createStore(rootReducer, initialState, enhancer);
