0cc5d6b27ace0d1b97a57239609e06691fcaff0c
Dmitriy <padavankun@gmail.com>
2017-02-28 11:09:27 +0300

    integrated tested ui. Added webpack loaders and font-awesome package

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,30 @@ module.exports = {
       test: /\.js$/,
       use: ['react-hot-loader', 'babel-loader'],
       include: path.join(__dirname, 'src'),
-    }]
+    },
+    {
+      test: /\.less$/,
+      use: ['style-loader', 
+      { loader: 'css-loader', options: { importLoaders: 1 } },
+      'less-loader']
+    },
+    {
+        test: /\.woff(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url-loader?limit=10000&mimetype=applfont-woff"
+    }, {
+        test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url-loader?limit=10000&mimetype=applfont-woff"
+    }, {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url-loader?limit=10000&mimetype=apploctet-stream"
+    }, {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "file-loader"
+    }, {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url-loader?limit=10000&mimetype=image/svg+xml"
+    },
+    ]
   }
 };
 
