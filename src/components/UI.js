d3d629f3782438625df0b34d9b3bfe529bad4549
Dmitriy <padavankun@gmail.com>
2017-02-28 19:00:47 +0300

    each icon from icon-bar toggle differente sidebar

--- src/components/UI.js
+++ src/components/UI.js
@@ -1,37 +1,50 @@
 import React from 'react';
 
 import 'font-awesome/less/font-awesome.less';
-import './UI.less'	
+import './UI.less'
+
+import ControlButtonsController from '../containers/ControlButtonsController';	
 
 
 class UI extends React.Component {
     constructor(props) {
         super(props);
-        this.toggle = this.toggle.bind(this);
         this.state = 
         {
             isOpen: false,
+            current: 0,
             style:{float:'right'},
         };
     }
   
-    toggle(e) {
-        e.preventDefault();
-        this.setState({ isOpen: !this.state.isOpen });
+
+    toggle = (id) => {
+    	const isOpen = !(this.state.current === id && this.state.isOpen === true); 
+    	this.setState({...this.state, isOpen, current:id});
     }
 
     render() {
+    		
         return (
+
 				<div><div className="icon-bar">
-            <a onClick={this.toggle} href="#"><i className="fa fa-home"></i></a> 
-            <a onClick={this.toggle} href="#"><i className="fa fa-search"></i></a> 
-            <a href="#"><i className="fa fa-clock-o"></i></a> 
-            <a href="#"><i className="fa fa-globe"></i></a>
-            <a href="#"><i className="fa fa-bar-chart"></i></a> 
+            <a id="Sidebar1" onClick={() => this.toggle('1')} href="#"><i className="fa fa-home"></i></a> 
+            <a onClick={() => this.toggle('2')} href="#"><i className="fa fa-search"></i></a> 
+            <a onClick={() => this.toggle('3')} href="#"><i className="fa fa-clock-o"></i></a> 
+            <a onClick={() => this.toggle('3')} href="#"><i className="fa fa-globe"></i></a>
+            <a onClick={() => this.toggle('3')} href="#"><i className="fa fa-bar-chart"></i></a> 
             <a href="#"><i className="fa fa-cog"></i></a> 
             <a href="#"><i className="fa fa-cog"></i></a> 
         </div>
-        { this.state.isOpen ? <SideBar /> : null }
+        
+        	{this.state.isOpen ?
+        	<div>
+         {this.state.current == 1 ? <SideBar /> : null }
+         {this.state.current == 2 ? <SideBar2 /> : null }
+         {this.state.current == 3 ? <SideBar3 /> : null }
+         </div> : null
+       }
+      
 				</div>
         );}
 }
@@ -41,11 +54,27 @@ class SideBar extends React.Component {
     render() {
         return(
         <div className="sidenav">
-          Red
-          <div className="panel panel-default">
-          <div className="panel-body"> <a> Event 1</a> </div>
-          </div>
-          </div>
+          ControlButtonsController
+          <ControlButtonsController />
+        </div>
+        );}
+ }
+
+class SideBar2 extends React.Component {
+    render() {
+        return(
+        <div className="sidenav">
+          SideBar 2
+        </div>
+        );}
+}
+
+class SideBar3 extends React.Component {
+    render() {
+        return(
+        <div className="sidenav">
+          SideBar 3
+        </div>
         );}
  }
 
