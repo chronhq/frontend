dfa864a6998ca4ced9f8e3e31e9f8c7f4b417e7a
Michael Orlov <MiklerGM@gmail.com>
2017-03-05 19:47:43 +0300

    Added zoom, scale and some styles :3

--- src/containers/MapD3Container.js
+++ src/containers/MapD3Container.js
@@ -1,10 +1,17 @@
 import React, { Component } from 'react';
 import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import * as d3 from 'd3';
 import Locations from './Locations';
 import { askBackend } from '../reducers/actions';
 
+import './MapD3Container.less';
+
 class MapD3Container extends Component {
+  state = {
+    zoomInitted: false,
+    transform: null
+  }
   componentDidMount() {
     this.props.askBackend('LOCATIONS');
     this.props.askBackend('LOCATIONS_TIMELINE');
@@ -17,21 +24,52 @@ class MapD3Container extends Component {
     this.props.askBackend('PERSONS_TIMELINE');
     this.props.askBackend('PERSONS_FACTS');
   }
+  componentDidUpdate() {
+    if (!this.state.zoomInitted) {
+      const svg = d3.select(this.svgMap);
+
+      svg.call(this.zoom);
+
+      this.setState({
+        zoomInitted: true
+      });
+    }
+  }
+
+  onZoom() {
+    this.setState({
+      transform: d3.event.transform
+    });
+  }
+
+  zoom = d3.zoom()
+    .scaleExtent([0.5, 10])
+    .on('zoom', this.onZoom.bind(this));
+
+  get transform() {
+    if (this.state.transform) {
+      const { x, y, k } = this.state.transform;
+      return `translate(${x}, ${y}) scale(${k})`;
+    }
+    return null;
+  }
 
   render() {
     return (
-      <svg className='svgMap'>
-        <g className='svgMapTerrain' key='terrain' strokeWidth="0.6" >
-          {Object.keys(this.props.terrain).map(continent => (
-            <path key={`terrain_${continent}`} d={this.props.terrain[continent]} />
-            ))
+      <svg className='svgMap' ref={(r) => { this.svgMap = r; }}>
+        <g transform={this.transform}>
+          <g className='svgMapTerrain' key='terrain' strokeWidth="0.6" >
+            {Object.keys(this.props.terrain).map(continent => (
+              <path key={`terrain_${continent}`} d={this.props.terrain[continent]} />
+              ))
+            }
+          </g>
+          { this.props.visibility.borders && <g>
+            <path className='svgMapBorders' d={this.props.borders} />
+          </g>
           }
+          <Locations />
         </g>
-        { this.props.visibility.borders && <g>
-          <path className='svgMapBorders' d={this.props.borders} />
-        </g>
-         }
-        <Locations />
       </svg>
     );
   }
