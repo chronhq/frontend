070390135490b47c3fb30bf8a00a4a8439dce3d1
Michael Orlov <MiklerGM@gmail.com>
2017-03-07 12:59:43 +0300

    Added Djima's TimePanel

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import MapD3Container from './containers/MapD3Container';
 import SidePanel from './components/SidePanel';
+import TimePanel from './components/TimePanel';
 
 import DevTools from './DevTools';
 
 const App = () => (
   <div>
     <SidePanel />
+    <TimePanel />
     <div className='content'>
       <MapD3Container />
     </div>
---
+++ src/components/TimePanel.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import * as d3 from 'd3';
+
+import './TimePanel.less';
+
+class Timeline extends React.Component{
+  state = {xClockPos:0}
+
+  componentDidMount() {
+    this.renderAxis();
+    let svgComp = d3.select("svg");
+
+    svgComp.on("click", () => {this.updateClockPosition();});
+    
+
+    svgComp.on("mousedown", () => {this.followMouse()});
+  
+
+  }
+  componentDidUpdate() {this.renderAxis() }
+
+  followMouse() {
+    console.log("mousedown");
+    d3.select('circle')
+      .on("mousemove", () => {this.updateClockPosition() })
+      .on("mouseup", () => {this.stopFollowing() })
+  }
+
+  stopFollowing() {
+      d3.select('circle')
+        .on("mousemove", null)
+        .on("mouseup", () => null)
+    }
+
+
+  updateClockPosition() {
+    
+    this.xClockPos = d3.mouse(this.svgTime)[0];
+    console.log(`Coords x is ${this.xClockPos}`);
+    d3.select('circle').attr("cx", this.xClockPos);
+  }
+
+
+  renderAxis() {
+     
+    const scale = d3.scaleLinear()
+      .domain([1700,2015])
+      .range([0,1000]);
+
+    const axis = d3.axisBottom(scale);
+
+    const svg = d3.select("svg")
+      .append('g')
+      .call(axis.ticks(20, "f"));
+      
+  svg.append("circle")
+    .attr("cx", this.xClockPos)
+    .attr('cy', 0)
+    .attr("r", 5)
+    .attr("class", "circle")
+    .style("fill", "red")
+    .attr("stroke", "steelblue")
+    .attr("stroke-width", 4);
+}
+
+
+
+  render() {
+    return (
+      <div id='timeline'>
+        <svg 
+          className="svgTime" 
+          ref={(r) => { this.svgTime = r; }} 
+          width="1100" height="50" 
+          viewBox="0 0 900 20"
+          preserveAspectRatio="xMidYMid meet">
+        </svg>
+      </div>
+    )
+  }
+}
+
+
+const TimelineTest = () => (
+  <div className='svgTime'> Test </div>
+);
+
+export default Timeline;
