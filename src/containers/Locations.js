51e2ac12f7c42658de790b81b7688d134240082c
Michael Orlov <MiklerGM@gmail.com>
2017-03-08 17:03:16 +0300

    Extract pin and tooltip for cleaner loop

--- src/containers/Locations.js
+++ src/containers/Locations.js
@@ -3,33 +3,38 @@ import { connect } from 'react-redux';
 import Pin from '../components/Pin';
 import PinTooltip from '../components/PinTooltip';
 
+const DrawPin = ({ city, visibility }) => (
+  <g key={`pin_list_${city.id}`}>
+    {visibility.locations &&
+      <Pin location={city} />
+    }
+    {visibility.tooltips &&
+      <PinTooltip location={city} />
+    }
+  </g>
+);
 
 class Locations extends Component {
-  getLocation(id) {
-    return {
-      id,
-      x: this.props.projected[id].x,
-      y: this.props.projected[id].y,
-      name: this.props.places[id].name,
-      scaleRank: (this.props.places[id].scaleRank * this.props.scale) / 100 / 3
-    }; // Default scale is 300 for point size
-  }
-  checkSize(id) {
-    return this.props.places[id].scaleRank < this.props.visibility.scale;
-  }
+  getLocation = id => ({
+    id,
+    x: this.props.projected[id].x,
+    y: this.props.projected[id].y,
+    name: this.props.places[id].name,
+    scaleRank: (this.props.places[id].scaleRank * this.props.scale) / 100 / 3
+  }); // Default scale is 300 for point size
+
+  checkSize = id => this.props.places[id].scaleRank < this.props.visibility.scale;
+
   render() {
     return (
       <g key='locations'>
         {this.props.current.map(city => (
-          this.checkSize(city) ?
-            <g key={`pin_list_${city}`}>
-              {this.props.visibility.locations &&
-                <Pin location={this.getLocation(city)} />
-              }
-              {this.props.visibility.tooltips &&
-                <PinTooltip location={this.getLocation(city)} />
-              }
-            </g>
+          this.checkSize(city)
+            ? <DrawPin
+              key={`pin_list_${city}`}
+              city={this.getLocation(city)}
+              visibility={this.props.visibility}
+            />
           : ''
         ))}
       </g>
