c6931bd74106c1d4636a82be4cee0f9162e1293a
Dmitriy <padavankun@gmail.com>
2017-03-29 09:01:47 +0300

    More debloat Survey. surveys fix.

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,15 +1,40 @@
 import React from 'react';
 import './Modal.less';
 
-const opt1_1 = 'учитель', opt1_2 = 'учащийся', opt1_3 ='научный работник', opt1_4 = 'укажите свой вариант: ';
-const opt2_1 = 'удобный' , opt2_2 = 'неудобный';
-const opt3_1 = 'регулярно', opt3_2 = 'время от времени', opt3_3 = 'больше никогда';
-const opt4_1 = 'да', opt4_2 = 'нет';
-const opt5_1 = 'да', opt5_2 = 'нет';
-const opt6_1 = 'да', opt6_2 = 'нет';
-const opt61_1 = ' развлечения', opt61_2 = 'рабочего процесса', opt61_3 = 'свой вариант';
-const opt7_1 = 'правильно', opt7_2 = 'неправильно, но я знаю как лучше';
-const opt8_1 = 'только новости про обновление сервиса', opt8_2 = 'новые опросы, я помогу сделать сервис лучше';
+const QuestionZero = 'Я';
+const OptionsZero = ['учитель', 'учащийся', 'научный работник', 'укажите свой вариант:'];
+
+const QuestionOne = 'Подобный формат отображения данных для меня:';
+const OptionsOne = ['удобный', 'неудобный'];
+const QuestionOneOne = 'Я бы добавил';
+const QuestionOneTwo = 'Я бы убрал';
+
+const QuestionTwo = 'Я считаю, что для моих нужд он:';
+const OptionsTwo = ['подходит', 'не подходит'];
+const QuestionTwoOne = 'Почему?';
+const QuestionTwoTwo = 'Мне не хватает';
+
+const QuestionThree = 'Я думаю, что буду пользоваться ресурсом';
+const OptionsThree = ['регулярно', 'время от времени', 'больше никогда'];
+
+const QuestionFour = 'Я рассматриваю возможность оформления платной подписки на сервис:';
+const OptionsFour = ['да', 'нет'];
+
+const QuestionFive = 'Я уже использую подобный сервис:';
+const OptionsFive = ['да', 'нет'];
+
+const QuestionSix = 'Мне бы хотелось показать сервис знакомым или коллегам';
+const OptionsSix = ['да', 'нет'];
+
+const QuestionSixOne = 'В качестве: ';
+const OptionsSixOne = ['развлечения', 'рабочего процесса', 'свой вариант'];
+
+const QuestionSeven = 'Вы всё делаете:';
+const OptionsSeven = ['правильно', 'неправильно, но я знаю как лучше'];
+
+const QuestionEight = 'Пришлите мне:';
+const OptionsEight = ['только новости про обновление сервиса', 'новые опросы, я помогу сделать сервис лучше'];
+
 
 class Radio extends React.Component {
   constructor(props) {
@@ -102,68 +127,68 @@ class Modal extends React.Component {
               </p>
 
               <form onSubmit={this.handleFormSubmit}>
-                Я
-                <Radio name='question1' value={opt1_1} data={this.state.answers.question1} cb={this.handleChange} />
-                <Radio name='question1' value={opt1_2} data={this.state.answers.question1} cb={this.handleChange} />
-                <Radio name='question1' value={opt1_3} data={this.state.answers.question1} cb={this.handleChange} />
-                <Radio name='question1' value={opt1_4} data={this.state.answers.question1} cb={this.handleChange} />
-                {this.state.answers.question1 === opt1_4 ?
-                  <input type="text" onChange={e => this.handleChange(e, 'question1why')} value={this.state.answers.question1why} />
+                {QuestionZero}
+                {OptionsZero.map(option => <Radio name='question0' value={option} key={option} data={this.state.answers.question0} cb={this.handleChange} />)}
+                {this.state.answers.question0 === OptionsZero[3] ?
+                  <input type="text" onChange={e => this.handleChange(e, 'question0why')} value={this.state.answers.question0why} />
                   : null}
                 <hr />
 
-                Подобный формат отображения данных для меня:
-                <Radio name='question2' value={opt2_1} data={this.state.answers.question2} cb={this.handleChange} />
-                <Radio name='question2' value={opt2_2} data={this.state.answers.question2} cb={this.handleChange} />
-                <p> Почему?
+                {QuestionOne}
+                {OptionsOne.map(option => <Radio name='question1' value={option} key={option} data={this.state.answers.question1} cb={this.handleChange} />)}
+                <p> {QuestionOneOne}
+                  <input type="text" onChange={e => this.handleChange(e, 'question11')} value={this.state.answers.question11} />
+                </p>
+                <p> {QuestionOneTwo}
+                  <input type="text" onChange={e => this.handleChange(e, 'question12')} value={this.state.answers.question12} />
+                </p>
+                <hr />
+
+
+                {QuestionTwo}
+                {OptionsTwo.map(option => <Radio name='question2' value={option} key={option} data={this.state.answers.question2} cb={this.handleChange} />)}
+                <p> {QuestionTwoOne}
                   <input type="text" onChange={e => this.handleChange(e, 'question21')} value={this.state.answers.question21} />
                 </p>
-                <p> Мне не хватает
-                 <input type="text" onChange={e => this.handleChange(e, 'question22')} value={this.state.answers.question22} />
+                <p> {QuestionTwoTwo}
+                  <input type="text" onChange={e => this.handleChange(e, 'question22')} value={this.state.answers.question22} />
                 </p>
                 <hr />
 
-                <p> Я думаю, что буду пользоваться ресурсом </p>
-                <Radio name='question3' value={opt3_1} data={this.state.answers.question3} cb={this.handleChange} />
-                <Radio name='question3' value={opt3_2} data={this.state.answers.question3} cb={this.handleChange} />
+                <p> {QuestionThree} </p>
+                {OptionsThree.map(option => <Radio name='question3' value={option} key={option} data={this.state.answers.question3} cb={this.handleChange} />)}
                 <hr />
 
-                <p> Я рассматриваю возможность оформления платной подписки на сервис: </p>
-                <Radio name='question4' value={opt4_1} data={this.state.answers.question4} cb={this.handleChange} />
-                <Radio name='question4' value={opt4_2} data={this.state.answers.question4} cb={this.handleChange} />
+                <p> {QuestionFour} </p>
+                {OptionsFour.map(option => <Radio name='question4' value={option} key={option} data={this.state.answers.question4} cb={this.handleChange} />)}
                 <hr />
 
-                <p> Я уже использую подобный сервис: </p>
-                <Radio name='question5' value={opt5_1} data={this.state.answers.question5} cb={this.handleChange} />
-                <Radio name='question5' value={opt5_2} data={this.state.answers.question5} cb={this.handleChange} />
+                <p> {QuestionFive} </p>
+                {OptionsFive.map(option => <Radio name='question5' value={option} key={option} data={this.state.answers.question5} cb={this.handleChange} />)}
                 <p> Если используете, перечислите пожалуйста: </p>
                 <input type="text" onChange={e => this.handleChange(e, 'question51')} value={this.state.answers.question51} />
                 <hr />
 
-                <p> Я рассматриваю возможность оформления платной подписки на сервис: </p>
-                <Radio name='question6' value={opt6_1} data={this.state.answers.question6} cb={this.handleChange} />
-                <Radio name='question6' value={opt6_2} data={this.state.answers.question6} cb={this.handleChange} />
-                <p> В качестве: </p>
-                <Radio name='question61' value={opt61_1} data={this.state.answers.question61} cb={this.handleChange} />
-                <Radio name='question61' value={opt61_2} data={this.state.answers.question61} cb={this.handleChange} />
-                <Radio name='question61' value={opt61_3} data={this.state.answers.question61} cb={this.handleChange} />
-                {this.state.answers.question61 === opt61_3 ?
+                <p> {QuestionSix} </p>
+                {OptionsSix.map(option => <Radio name='question6' value={option} key={option} data={this.state.answers.question6} cb={this.handleChange} />)}
+                <p> {QuestionSixOne} </p>
+                {OptionsSixOne.map(option => <Radio name='question61' value={option} key={option} data={this.state.answers.question61} cb={this.handleChange} />)}
+                {this.state.answers.question61 === OptionsSix[2] ?
                   <input type="text" onChange={e => this.handleChange(e, 'question61why')} value={this.state.answers.question61why} />
                 : null}
                 <hr />
 
-                <p> Вы всё делаете: </p>
-                <Radio name='question7' value={opt7_1} data={this.state.answers.question7} cb={this.handleChange} />
-                <Radio name='question7' value={opt7_2} data={this.state.answers.question7} cb={this.handleChange} />
-                {this.state.answers.question7===opt7_2 ?
-                    <input type="text" onChange={e => this.handleChange(e, 'question7why')} value={this.state.answers.question7why} />
+                <p> {QuestionSeven} </p>
+                {OptionsSeven.map(option => <Radio name='question7' value={option} key={option} data={this.state.answers.question7} cb={this.handleChange} />)}
+                {this.state.answers.question7 === OptionsSeven[1] ?
+                  <input type="text" onChange={e => this.handleChange(e, 'question7why')} value={this.state.answers.question7why} />
                     : null}
                 <hr />
 
 
-                <p> Пришлите мне: </p>
-                <Radio name='question8' value={opt8_1} data={this.state.answers.question8} cb={this.handleChange} />
-                <Radio name='question8' value={opt8_2} data={this.state.answers.question8} cb={this.handleChange} />
+                <p> {QuestionEight} </p>
+                {OptionsEight.map(option => <Radio name='question8' value={option} key={option} data={this.state.answers.question8} cb={this.handleChange} />)}
+
                 Пожалуйста, оставьте свой контакт для связи.
 
                 <button className="btn btn-default pull-right" type="submit"> Отправить </button>
