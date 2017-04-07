ae665493545ce94a1dec2048d9eb101d3cbf46cd
Michael Orlov <MiklerGM@gmail.com>
2017-04-07 15:25:44 +0300

    disable logging for production

--- shared/logger.js
+++ shared/logger.js
@@ -1,8 +1,21 @@
 export const wrapMessage = str => `${Date.now()} => ${str}`;
 
-export const logger = {
+const dummyOut = () => null;
+
+const debugLogger = {
   err: str => console.error(wrapMessage(str)),
   info: str => console.info(wrapMessage(str)),
-  log: str => console.error(wrapMessage(str)),
+  log: str => console.log(wrapMessage(str)),
   json: str => console.log(wrapMessage(JSON.stringify(str)))
 };
+
+const prodLogger = {
+  err: str => console.error(wrapMessage(str)),
+  info: dummyOut,
+  log: dummyOut,
+  json: dummyOut
+};
+
+const logger = process.env.NODE_ENV === 'production' ? prodLogger : debugLogger;
+
+export { logger };
