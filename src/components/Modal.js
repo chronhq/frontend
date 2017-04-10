cd6dd16ea9c0ecba8f8116909a6392f92054965c
Dmitriy <padavankun@gmail.com>
2017-04-10 19:08:20 +0300

    survey from json magic

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,43 +1,10 @@
 import React from 'react';
-import './Modal.less';
-
-import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import './Modal.less';
+// import SurveyList from './survey.json';
 import { askBackend } from '../reducers/actions';
-
-const QuestionZero = 'Я';
-const OptionsZero = ['учитель', 'учащийся', 'научный работник', 'укажите свой вариант:'];
-
-const QuestionOne = 'Подобный формат отображения данных для меня:';
-const OptionsOne = ['удобный', 'неудобный'];
-const QuestionOneOne = 'Я бы добавил';
-const QuestionOneTwo = 'Я бы убрал';
-
-const QuestionTwo = 'Я считаю, что для моих нужд он:';
-const OptionsTwo = ['подходит', 'не подходит'];
-const QuestionTwoOne = 'Почему?';
-const QuestionTwoTwo = 'Мне не хватает';
-
-const QuestionThree = 'Я думаю, что буду пользоваться ресурсом';
-const OptionsThree = ['регулярно', 'время от времени', 'больше никогда'];
-
-const QuestionFour = 'Я рассматриваю возможность оформления платной подписки на сервис:';
-const OptionsFour = ['да', 'нет'];
-
-const QuestionFive = 'Я уже использую подобный сервис:';
-const OptionsFive = ['да', 'нет'];
-
-const QuestionSix = 'Мне бы хотелось показать сервис знакомым или коллегам';
-const OptionsSix = ['да', 'нет'];
-
-const QuestionSixOne = 'В качестве: ';
-const OptionsSixOne = ['развлечения', 'рабочего процесса', 'свой вариант'];
-
-const QuestionSeven = 'Вы всё делаете:';
-const OptionsSeven = ['правильно', 'неправильно, но я знаю как лучше'];
-
-const QuestionEight = 'Пришлите мне:';
-const OptionsEight = ['только новости про обновление сервиса', 'новые опросы, я помогу сделать сервис лучше'];
 
 
 class Radio extends React.Component {
@@ -71,12 +38,14 @@ Radio.propTypes = {
   value: React.PropTypes.string.isRequired,
   data: React.PropTypes.string,
   cb: React.PropTypes.func.isRequired,
-  name: React.PropTypes.string.isRequired,
+  name: React.PropTypes.number,
 };
 
 Radio.defaultProps = {
   data: '',
+  name: '',
 };
+
 
 class Modal extends React.Component {
   constructor(props) {
@@ -94,13 +63,14 @@ class Modal extends React.Component {
     };
 
     this.setState({ answers: newAnswers });
-    console.log(`You have selected ${newAnswers} `);
+    // console.log(`You have selected ${newAnswers} `);
   }
 
   handleFormSubmit = (e) => {
     e.preventDefault();
     console.log(`You have selected ${this.state.answers} `);
     // this.props.askBackend('');
+    this.props.askBackend('SURVEYS_ANSWER', this.state.answers);
   }
 
   close(e) {
@@ -110,95 +80,63 @@ class Modal extends React.Component {
     }
   }
 
-  render() {
-    if (this.props.isOpen === false) {
-      return null;
-    }
+  processOptions = (option, oid, qid) => {
+    switch (option.type) {
+      case 'radio':
+        return (
+          <div key={oid}>
+            <Radio name={qid} value={option.value} key={`${qid}_${oid}`} data={this.state.answers[qid]} cb={this.handleChange} />
+          </div>
+        );
 
+      case 'radio-text':
+        return (
+          <div key={oid}>
+            <Radio name={qid} value={option.value} key={`${qid}_${oid}`} data={this.state.answers[qid]} cb={this.handleChange} />
+            {this.state.answers[qid] === option.value ?
+              <input type='text' onChange={e => this.handleChange(e, `${qid}_{oid}`)} value={this.state.answers[`${qid}_plus`]} />
+              : null}
+          </div>
+        );
+
+      case 'text':
+        return (
+          <div>
+            <p> {option.value} </p>
+            <input type='text' onChange={e => this.handleChange(e, `${qid}_${oid}`)} value={this.state.answers[`${qid}_${oid}`]} />;
+          </div>
+        );
+
+      default:
+        return null;
+    }
+  }
+
+
+  processQuestion = (data, qid) => (
+    <div key={qid}>
+      <p> {data.question} </p>
+      {data.options.map((cur, id) => this.processOptions(cur, id, qid))}
+    </div>
+  );
+
+  render() {
     return (
       <div>
         <div className='overlay' />
         <div className='modal-dialog modal-lg'>
           <div className='survey'>
-
             <div className="modal-header">
               <button type="button" className="close" data-dismiss="modal" onClick={e => this.close(e)} >&times;</button>
               <h4 className="modal-title pul5l-left">Опрос</h4>
             </div>
             <div className='modal-body'>
               <p>Нам важно, чтобы вы оценили свой опыт от использования сервиса.
-                Пожалуйста, после тестирования заполните небольшую анкету,
-                которая сделает продукт лучше.
+                 Пожалуйста, после тестирования заполните небольшую анкету,
+                 которая сделает продукт лучше.
               </p>
-
               <form onSubmit={this.handleFormSubmit}>
-                {QuestionZero}
-                {OptionsZero.map(option => <Radio name='question0' value={option} key={option} data={this.state.answers.question0} cb={this.handleChange} />)}
-                {this.state.answers.question0 === OptionsZero[3] ?
-                  <input type="text" maxLength='20' height='20' width='80' onChange={e => this.handleChange(e, 'question0why')} value={this.state.answers.question0why} />
-                  : null}
-                <hr />
-
-                {QuestionOne}
-                {OptionsOne.map(option => <Radio name='question1' value={option} key={option} data={this.state.answers.question1} cb={this.handleChange} />)}
-                <p> {QuestionOneOne}
-                  <input type="text" maxLength='2000' onChange={e => this.handleChange(e, 'question11')} value={this.state.answers.question11} />
-                </p>
-                <p> {QuestionOneTwo}
-                  <input type="text" maxLength='2000' onChange={e => this.handleChange(e, 'question12')} value={this.state.answers.question12} />
-                </p>
-                <hr />
-
-
-                {QuestionTwo}
-                {OptionsTwo.map(option => <Radio name='question2' value={option} key={option} data={this.state.answers.question2} cb={this.handleChange} />)}
-                <p> {QuestionTwoOne}
-                  <input type="text" onChange={e => this.handleChange(e, 'question21')} value={this.state.answers.question21} />
-                </p>
-                <p> {QuestionTwoTwo}
-                  <input type="text" onChange={e => this.handleChange(e, 'question22')} value={this.state.answers.question22} />
-                </p>
-                <hr />
-
-                <p> {QuestionThree} </p>
-                {OptionsThree.map(option => <Radio name='question3' value={option} key={option} data={this.state.answers.question3} cb={this.handleChange} />)}
-                <hr />
-
-                <p> {QuestionFour} </p>
-                {OptionsFour.map(option => <Radio name='question4' value={option} key={option} data={this.state.answers.question4} cb={this.handleChange} />)}
-                <hr />
-
-                <p> {QuestionFive} </p>
-                {OptionsFive.map(option => <Radio name='question5' value={option} key={option} data={this.state.answers.question5} cb={this.handleChange} />)}
-                <p> Если используете, перечислите пожалуйста: </p>
-                <input type="text" onChange={e => this.handleChange(e, 'question51')} value={this.state.answers.question51} />
-                <hr />
-
-                <p> {QuestionSix} </p>
-                {OptionsSix.map(option => <Radio name='question6' value={option} key={option} data={this.state.answers.question6} cb={this.handleChange} />)}
-                <p> {QuestionSixOne} </p>
-                {OptionsSixOne.map(option => <Radio name='question61' value={option} key={option} data={this.state.answers.question61} cb={this.handleChange} />)}
-                {this.state.answers.question61 === OptionsSix[2] ?
-                  <input type="text" onChange={e => this.handleChange(e, 'question61why')} value={this.state.answers.question61why} />
-                : null}
-                <hr />
-
-                <p> {QuestionSeven} </p>
-                {OptionsSeven.map(option => <Radio name='question7' value={option} key={option} data={this.state.answers.question7} cb={this.handleChange} />)}
-                {this.state.answers.question7 === OptionsSeven[1] ?
-                  <input type="text" onChange={e => this.handleChange(e, 'question7why')} value={this.state.answers.question7why} />
-                    : null}
-                <hr />
-
-
-                <p> {QuestionEight} </p>
-                {OptionsEight.map(option => <Radio name='question8' value={option} key={option} data={this.state.answers.question8} cb={this.handleChange} />)}
-                <hr />
-                Пожалуйста, оставьте свой контакт для связи.
-
-                <input type="text" placeholder="Ваш E-mail" onChange={e => this.handleChange(e, 'mail')} value={this.state.answers.mail} />
-                <hr />
-
+                {this.props.surveys[1].json.map(this.processQuestion)}
                 <button className="btn btn-default pull-right" type="submit"> Отправить </button>
               </form>
             </div>
@@ -212,6 +150,19 @@ class Modal extends React.Component {
 Modal.propTypes = {
   onClose: React.PropTypes.func.isRequired,
   isOpen: React.PropTypes.bool.isRequired,
+  askBackend: React.PropTypes.func.isRequired,
+  surveys: React.PropTypes.object.isRequired,
 };
 
-export default Modal;
+
+function mapStateToProps(state) {
+  return { surveys: state.surveys.byId };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    askBackend: bindActionCreators(askBackend, dispatch)
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Modal);
