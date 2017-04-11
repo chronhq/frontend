cfc39c6aab37b88f95547a3be91ed89e2617e0c9
Dmitriy <padavankun@gmail.com>
2017-04-11 14:17:55 +0300

    Intro fixes

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -27,6 +27,10 @@ class Intro extends React.Component {
     this.setState({ whichSlide: e });
   }
 
+  leftArrow = () => {
+    return <div className='arrowbtn'>Button</div>;
+  };
+
   render() {
     if (this.props.isOpen === false) {
       return null;
@@ -35,8 +39,9 @@ class Intro extends React.Component {
     const settings = {
       dots: true,
       autoplay: true,
-      autoplaySpeed: 8000,
-      arrows: false,
+      autoplaySpeed: 800,
+      arrows: true,
+      adaptiveHeight: false,
       fade: true,
       infinite: true,
       lazyLoad: true,
@@ -44,16 +49,19 @@ class Intro extends React.Component {
       beforeChange: this.nextClick,
       slidesToShow: 1,
       slidesToScroll: 1,
-      pauseOnHover: false
+      pauseOnHover: true
     };
 
     const SlideSalutation = 'Вы попали в демо-версию сервиса "Хронист", который создаётся для того, чтобы помочь в учёбе, исследованиях или просто весело и с пользой провести время.';
-    const SlideAll = '«Хронист» - это мультимедийный атлас, благодаря которому вы сами выбираете типы данных, которые нужно отобразить, и сами настраиваете хронологию, которая вам интересна. Мы хотим показать, что изучение истории становится очень занимательным, когда вы изучаете не один регион, а всю планету. Что история всего мира взаимосвязана и вы можете самостоятельно находить и изучать эти взаимосвязи.';
-    const SlideSurvey = 'Нам важно, чтобы вы оценили свой опыт от использования сервиса. Пожалуйста, после тестирования заполните небольшую анкету, которая сделает продукт лучше.';
     const SlideDemo = 'В демо версии вы найдёте лишь небольшую активную часть карты (она обозначена цветом) и отрезок времени, который сможете использовать. Мы постоянно работаем над расширением нашей базы и в сентябре откроем карту целиком (вы всегда можете проследить сколько данных мы добавили, счётчик есть на главной странице). ';
-    const SlideLayers = 'Мы также работаем над добавлением «слоёв». Слой - это набор данных определённого типа - население, стихийные бедствия, религиозные направления, политическое устройство и тд. Сейчас мы предлагаем всего два слоя: города и политические границы. ';
+    const SlideLegend = 'Легенда находится на боковой панели.';
+    const SlideAll = '«Хронист» - это мультимедийный атлас, благодаря которому вы сами выбираете типы данных, которые нужно отобразить, и сами настраиваете хронологию, которая вам интересна. Мы хотим показать, что изучение истории становится очень занимательным, когда вы изучаете не один регион, а всю планету. Что история всего мира взаимосвязана и вы можете самостоятельно находить и изучать эти взаимосвязи.';
+    const SlideTimeControl = 'Следить за изменениями можно перемещая указатель по временной шкале как самостоятельно, так и при помощи кнопок управления.';
+    const SlideSurvey = 'Нам важно, чтобы вы оценили свой опыт от использования сервиса. Пожалуйста, после тестирования заполните небольшую анкету, которая сделает продукт лучше.';
+    const SlideLayers = 'Мы также работаем над добавлением «слоёв». Слой - это набор данных определённого типа - население, стихийные бедствия, религиозные направления, политическое устройство и тд. Сейчас мы предлагаем всего два слоя: города и политические границы. Слой «изобретения» по умолчанию включен и отображается как лента событий. При наведении курсора на событие оно отмечается в городе, в котором произошло. Изобретения также можно выделить щелчком и экспортировать в текстовый формат. Их также можно будет сортировать, экспортировать в различном варианте и переходить к полной версии со ссылками на научные материалы.  ';
     const SlideInventions = 'Слой «изобретения» по умолчанию включен и отображается как лента событий. При наведении курсора на событие оно отмечается в городе, в котором произошло. Изобретения также можно выделить щелчком и экспортировать в текстовый формат.';
     const SlideInvetionsAgain = 'В скором времени «изобретения» переместятся в другую вкладку, их можно будет отключать и накладывать на другие слои. В таком случае, вероятно, некоторые явления приобретут причинно-следственную связь. Их также можно будет сортировать, экспортировать в различном варианте и переходить к полной версии со ссылками на научные материалы.';
+    const SlideHome = 'Вы можете вернуться к интро, нажав "домой" на боковой панели.';
 
     const highlightDiv = {
       backgroundColor: 'rgba(0,0,0,0.0)',
@@ -64,23 +72,26 @@ class Intro extends React.Component {
 
     return (
       <div className='overlay'>
-        <div style={this.state.whichSlide === 1 ? highlightDiv : null} className='overlay-iconbar' />
-        <div style={this.state.whichSlide === 2 ? highlightDiv : null} className='overlay-survey' />
-        <div style={this.state.whichSlide === 5 ? highlightDiv : null} className='overlay-iconbar' />
-        <div style={this.state.whichSlide === 1 ? highlightDiv : null} className='overlay-timeline' />
-        <div style={this.state.whichSlide === 6 ? highlightDiv : null} className='overlay-invention' />
+        <div style={this.state.whichSlide === 0 ? highlightDiv : null} className='overlay-iconbar' />
+        <div style={this.state.whichSlide === 0 ? highlightDiv : null} className='overlay-timeline' />
+        <div style={this.state.whichSlide === 2 ? highlightDiv : null} className='overlay-legend' />
+        <div style={this.state.whichSlide === 3 ? highlightDiv : null} className='overlay-timecontrol' />
+        <div style={this.state.whichSlide === 4 ? highlightDiv : null} className='overlay-invention' />
+        <div style={this.state.whichSlide === 4 ? highlightDiv : null} className='overlay-feed' />
+        <div style={this.state.whichSlide === 5 ? highlightDiv : null} className='overlay-survey' />
+        <div style={this.state.whichSlide === 6 ? highlightDiv : null} className='overlay-home' />
 
         <div className='row'>
           <div className='containter center'>
             <div className='slider'>
               <Slider class='text-center' {...settings}>
-                <div><h3> {SlideSalutation} </h3></div>
-                <div><h3> {SlideAll} </h3></div>
-                <div><h3> {SlideSurvey} </h3></div>
-                <div><h3> {SlideDemo} </h3></div>
-                <div><h3> {SlideLayers} </h3></div>
-                <div><h3> {SlideInventions} </h3></div>
-                <div><h3> {SlideInvetionsAgain} </h3></div>
+                <div><h4> {SlideSalutation} </h4></div>
+                <div><h4> {SlideDemo} </h4></div>
+                <div><h4> {SlideLegend} </h4></div>
+                <div><h4> {SlideTimeControl} </h4></div>
+                <div><h4> {SlideLayers} </h4></div>
+                <div><h4> {SlideSurvey} </h4></div>
+                <div><h4> {SlideHome} </h4></div>
               </Slider>
               <button onClick={e => this.close(e)} className='btn btn-primary'> OK </button>
             </div>
