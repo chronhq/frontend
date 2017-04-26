6bc69c935da9e2f834df79e4208f01bc21daad7b
Michael Orlov <MiklerGM@gmail.com>
2017-04-26 19:13:11 +0300

    Fix floating away city names after zoom

--- src/containers/Locations/PinTooltip.js
+++ src/containers/Locations/PinTooltip.js
@@ -7,9 +7,15 @@ const textSize = (scaleRank, scale) => {
     : font / Math.sqrt((Number(scaleRank)));
 };
 
+const textX = (location, scale) => {
+  const shift = 0.3;
+  const x = textSize(location.scaleRank, scale) * location.name.length * shift;
+  return `${x}px`;
+}
+
 const PinTooltip = ({ location, scale }) => (
   <g key={`pin_tooltip_${location.cityId}`}className='tooltip' transform={`translate(${location.x},${location.y})`}>
-    <text dy='1em' x='2em' textAnchor='middle' fontSize={textSize(location.scaleRank, scale)}>
+    <text dy='1em' x={textX(location, scale)} textAnchor='middle' fontSize={textSize(location.scaleRank, scale)}>
       {location.name}
     </text>
   </g>
