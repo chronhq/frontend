ef4f00c5433335b48857e3c2d67f19b7c40ab3bd
Dmitriy <padavankun@gmail.com>
2017-08-15 14:40:03 +0300

    package updates. Sidebar-css modification. debugpanel. modaljs removal

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { OverlayTrigger, Tooltip, ButtonToolbar, Button } from 'react-bootstrap';
+import { OverlayTrigger, Tooltip, ButtonToolbar, Button, FormControl, ControlLabel } from 'react-bootstrap';
 
 import { YMInitializer } from 'react-yandex-metrika';
 import ym from 'react-yandex-metrika';
 
 import 'font-awesome/less/font-awesome.less';
 
-
 import Settings from '../containers/Settings';
 import Feed from '../containers/Feed';
 import Legend from '../containers/Legend';
 import Intro from './Intro';
 import Feedback from './Feedback/';
-import Modal from './Modal';
+import ControlButtons from '../components/ControlButtons';
 
 import './SidePanel.less';
 
@@ -23,17 +22,63 @@ const tooltip = text => (
   <Tooltip id="tooltip"><strong>{text}</strong></Tooltip>
 );
 
+class AlignToggler extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: 'right',
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ value: event.target.value });
+    console.log(this.state.value);
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.cb(`${this.state.value}`);
+  }
+
+  render() {
+    return (
+      <div className='text-center'>
+        <h3> Debug </h3>
+        <form onSubmit={e => this.handleSubmit(e)}>
+          <ControlLabel>UI Align</ControlLabel>
+          <FormControl componentClass="select" placeholder="select" onChange={e => this.handleChange(e)}>
+            <option value='right'>Правое</option>
+            <option value='left'>Левое</option>
+            <option value='none'>none</option>
+          </FormControl>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
+    );
+  }
+}
+
 class SidePanel extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       isOpen: false,
       current: 0,
-      style: { float: 'right' },
       isIntroOn: (process.env.NODE_ENV === 'production'),
       isFeedbackOn: false,
+      UI: { alignPanel: 'right' }
     };
   }
+
+  // get iconBarStyle() {
+  //   return (
+  //       position: 'fixed',
+  //       this.state.alignPanel == left ? 'right: 0' : 'left: 0',
+  //       float: 'right',
+  //       top: 0,
+  //       width: '50px',
+  //   );
+  // }
 
   componentDidMount() {
     this.logPageView();
@@ -59,6 +104,19 @@ class SidePanel extends React.Component {
     this.setState({ isFeedbackOn: !this.state.isFeedbackOn });
   }
 
+  handleChange = (type, data) => {
+    // console.log('have new data in handle change', data);
+    const UI = {
+      ...this.state.UI,
+    };
+      // [type]: { ...this.state.UI[type], ...data }
+    UI[type] = data;
+    // console.log(data);
+    this.setState({ UI });
+    // this.forceUpdate();
+  }
+
+
   FeedbackButton = () => (
     <div className='export-buttons'>
       <Button bsStyle='default' onClick={() => this.toggleFeedback()}><i className='fa fa-comment fa-fw' />Кнопка</Button>
@@ -69,19 +127,17 @@ class SidePanel extends React.Component {
     return (
 
       <div>
-
         <YMInitializer accounts={YmId} options={{ defer: true }} />
-        <div className="icon-bar" style={this.onTopStyle}>
+        <div className={this.state.UI.alignPanel === 'left' ? 'icon-bar--left icon-bar' : 'icon-bar--right icon-bar'} >
           <ButtonToolbar>
             <OverlayTrigger placement='left' delayHide={0} overlay={tooltip('Интро')} >
               <Button bsStyle='default' onClick={() => this.toggleIntro()}><i className='fa fa-home fa-fw' /> </Button>
-              {/* <Button bsStyle='default' onClick={() => this.toggle(1)}><i className='fa fa-home fa-fw' /> </Button> */}
             </OverlayTrigger>
             <OverlayTrigger placement='left' delayHide={0} overlay={tooltip('Поиск')}>
               <Button bsStyle='default' onClick={() => this.toggle(2)}><i className='fa fa-search fa-fw' /></Button>
             </OverlayTrigger>
 
-            <OverlayTrigger placement='left' delayHide={0}  overlay={tooltip('Лента событий')}>
+            <OverlayTrigger placement='left' delayHide={0} overlay={tooltip('Лента событий')}>
               <Button bsStyle='default' onClick={() => this.toggle(3)}><i className='fa fa-list-ul fa-fw' /></Button>
             </OverlayTrigger>
             <OverlayTrigger placement='left' delayHide={0} overlay={tooltip('Легенда')}>
@@ -91,6 +147,12 @@ class SidePanel extends React.Component {
               <Button bsStyle='default' onClick={() => this.toggle(5)}><i className='fa fa-cog fa-fw' /></Button>
             </OverlayTrigger>
 
+            {(process.env.NODE_ENV !== 'production') &&
+            <OverlayTrigger placement='left' delayHide={0} overlay={tooltip('Debug')}>
+              <Button bsStyle='default' onClick={() => this.toggle(6)}><i className='fa fa-cog fa-fw' /></Button>
+            </OverlayTrigger>
+            }
+
           </ButtonToolbar>
         </div>
 
@@ -99,13 +161,20 @@ class SidePanel extends React.Component {
         <Feedback isOpen={this.state.isFeedbackOn} onClose={() => this.toggleFeedback()} />
 
         {this.state.isOpen &&
-          <div className="sidenav">
+          <div className={this.state.UI.alignPanel === 'left' ? 'sidenav--left sidenav' : 'sidenav--right sidenav'} >
             {this.state.current === 9 && <div> Empty</div> }
             {this.state.current === 2 && <SearchPanel /> }
             {this.state.current === 3 && <Feed /> }
             {this.state.current === 4 && <Legend /> }
             {this.state.current === 5 &&
               <Settings onClose={() => this.toggleFeedback()} />
+            }
+            {this.state.current === 7 && null }
+            {this.state.current === 6 &&
+              <div>
+                <AlignToggler cb={data => this.handleChange('alignPanel', data)} />
+                <ControlButtons />
+              </div>
             }
           </div>
        }
