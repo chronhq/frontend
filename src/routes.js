0229a0d36cbdc663244330f1f87a08c3911b2345
Dmitriy <padavankun@gmail.com>
2017-08-22 23:07:07 +0300

    fix router-loop

--- src/routes.js
+++ src/routes.js
@@ -4,58 +4,64 @@ import {
   Switch
 } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { bindActionCreators } from 'redux';
 
 import App from './App';
 // import NotFound from './'
-import StartPage from './components/StartPage';
-import { loadData } from './reducers/actions';
-
-const listOfCourses = [{
-  resource: 'COURSES',
-  req: {
-    filter: JSON.stringify({ where: { active: true } }),
-  },
-}];
+// import StartPage from './components/StartPage';
 
 
-const mapStateToAppProps = (state) => ({
-  // location: state.location
-  courses: state.courses.list.byId
-})
 
-const mapDispatchToProps = (dispatch) => ({
-    loadData: bindActionCreators(loadData, dispatch),
-  }
-);
+// import { bindActionCreators } from 'redux';
+// import { loadData } from './reducers/actions';
 
-// this.props.courses.list.byId.
-const AppRouter = connect(mapStateToAppProps, mapDispatchToProps)((props) =>
-{
-  // console.log(props);
-  // props.loadData(listOfCourses);
+// const listOfCourses = [{
+//   resource: 'COURSES',
+//   req: {
+//     filter: JSON.stringify({ where: { active: true } }),
+//   },
+// }];
 
-  return (
-  <div>
+
+// const mapStateToAppProps = (state) => ({
+//   // location: state.location
+//   courses: state.courses.list.byId
+// })
+
+// const mapDispatchToProps = (dispatch) => ({
+//     loadData: bindActionCreators(loadData, dispatch),
+//   }
+// );
+
+// // this.props.courses.list.byId.
+// const AppRouter = connect(mapStateToAppProps)((props) =>
+// {
+//   // console.log(props);
+//   // props.loadData(listOfCourses);
+
+//   return (
+//     <Switch>
+//       {/* <Route exact path={`/`} render={() => <StartPage id={props.courses}/>} /> */}
+//        <Route exact path='/' render={() => <App />} />
+//       {
+//       {Object.keys(props.courses).map(id =>
+//       )}
+//        }
+//        {/*
+//       <Route path={`/1`} render={() => <App /> } />
+//       <Route exact path={`/`} render={() => <StartPage id={1}/>} />
+//        <Route path='/course1' component={} />
+//        <Route render={NotFound} />
+//       */}
+//     </Switch>
+//   );
+// }
+// );
+
+const AppRouter = () => (
     <Switch>
-      {/* <Route exact path={`/`} render={() => <StartPage id={props.courses}/>} /> */}
        <Route exact path='/' render={() => <App />} />
-      {/*
-      {Object.keys(props.courses).map(id =>
-      )}
-       */}
-       {/*
-      <Route path={`/1`} render={() => <App /> } />
-      <Route exact path={`/`} render={() => <StartPage id={1}/>} />
-       <Route path='/course1' component={} />
-       <Route render={NotFound} />
-      */}
     </Switch>
-  </div>
-  );
-}
 );
-
 
 // class AppRouter extends AnotherClass {
 //   constructor(props) {
