5885fc330a3c6309aa6bf0e62d64bac4cd463845
Dmitriy <padavankun@gmail.com>
2017-09-15 12:46:47 +0300

    markup changes for timeline. ym update. basic hotkeys and new dependency for animation

--- src/components/Timeline/Timeline.js
+++ src/components/Timeline/Timeline.js
@@ -3,29 +3,36 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { changeTick } from '../../reducers/actions';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
 import './Timeline.less';
 
 
-const Event = ({ tickid, timeline, events, selectCb, selectedId }) => (
-  <div>
-    {events && events.map(event =>
-      <div
-        key={`event_${event.id}`}
-        // onClick={() => selectCb(event.id)}
-        onClick={() => selectCb(tickid, event.id)}
-        className={(selectedId == tickid) ? 'timeline__entry timeline__entry--selected' : 'timeline__entry'}
-      >
-        <div className="timeline__heading">
-          <h4 className='event__name'> {event.title} </h4>
-          <h4 className='event__date'> {timeline.year} </h4>
+const Event = ({ tickid, timeline, events, selectCb, selectedId }) => {
+  const eventClasses = ['timeline__entry'];
+  if (selectedId == tickid) { eventClasses.push('timeline__entry--selected'); }
+
+  return (
+    <div>
+      {events && events.map(event =>
+        <div
+          key={`event_${event.id}`}
+          // onClick={() => selectCb(event.id)}
+          onClick={() => selectCb(tickid, event.id)}
+          className={eventClasses.join(' ')}
+          role="button"
+        >
+          <div className="timeline__heading">
+            <h4 className='event__name'> {event.title} </h4>
+            <h4 className='event__date'> {timeline.year} </h4>
+          </div>
+          <div className='timeline__text'>
+            <p>{event.description}</p>
+          </div>
         </div>
-        <div className="timeline__text">
-          <p>{event.description}</p>
-        </div>
-      </div>
-    )}
-  </div>
-);
+      )}
+    </div>
+  );
+};
 
 class Timeline extends React.Component {
   static propTypes = {
@@ -35,7 +42,6 @@ class Timeline extends React.Component {
     tick: PropTypes.number.isRequired
   }
 
-
   handleSelect = (tick, event) => {
     console.log(`tick is ${tick}`);
     console.log(`event is ${event}`);
@@ -44,20 +50,49 @@ class Timeline extends React.Component {
     // this.props.selectLocation(eventId);
   }
 
+  handlePress(e) {
+    // console.log(e.keyCode);
+    e.preventDefault();
+    switch (e.keyCode) {
+      case 38:
+        this.handlePrevious();
+        break;
+      case 40:
+        this.handleNext();
+        break;
+    }
+  }
+
+  handleNext() {
+    this.props.timeline[parseInt(this.props.tick) + 1] && this.props.changeTick(parseInt(this.props.tick) + 1);
+  }
+
+  handlePrevious() {
+    this.props.timeline[this.props.tick - 1] && this.props.changeTick(this.props.tick - 1);
+  }
+
   render() {
     return (
-      <div className='timeline'>
-        {Object.keys(this.props.timeline).map(tickid =>
-          <Event
-            key={`events_${tickid}`}
-            tickid={tickid}
-            timeline={this.props.timeline[tickid]}
-            events={this.props.events[tickid]}
-            selectCb={(tick, event) => this.handleSelect(tick, event)}
-            selectedId={this.props.tick}
-          />
-        )}
-      </div>
+      <ReactCSSTransitionGroup>
+        <div
+          className='timeline'
+          id="keyboard"
+          role="button"
+          tabIndex='0'
+          onKeyDown={e => this.handlePress(e)}
+        >
+          {Object.keys(this.props.timeline).map(tickid =>
+            <Event
+              key={`events_${tickid}`}
+              tickid={tickid}
+              timeline={this.props.timeline[tickid]}
+              events={this.props.events[tickid]}
+              selectCb={(tick, event) => this.handleSelect(tick, event)}
+              selectedId={this.props.tick}
+            />
+          )}
+        </div>
+      </ReactCSSTransitionGroup>
     );
   }
 }
