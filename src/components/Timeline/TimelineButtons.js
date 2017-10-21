ce42361173c317c446eb4eb571a621e2a07ac7f7
Dmitriy <padavankun@gmail.com>
2017-10-21 14:42:42 +0300

    back button fix

--- src/components/Timeline/TimelineButtons.js
+++ src/components/Timeline/TimelineButtons.js
@@ -3,26 +3,48 @@ import './TimelineButtons.less';
 import { OverlayTrigger, Tooltip } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import BioButton from './BioButton';
+import { withRouter } from 'react-router-dom'
+import { setFlagsAction } from 'flag';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
 
 const tooltip = text => (
   <Tooltip id='tooltip'><strong>{text}</strong></Tooltip>
 );
 
-const ReturnButton = () => (
-  <OverlayTrigger placement='bottom' delayShow={150} delayHide={5} overlay={tooltip('Домой')} >
-    <Link to='/'>
-      <button>
-        <i className='fa fa-reply' aria-hidden='true' />
-      </button>
-    </Link>
-  </OverlayTrigger>
-);
+class ReturnButton extends React.Component {
+  handleClick() {
+    this.props.setFlagsAction({ CourseSelection: true });
+    this.props.history.push('/');
+    console.log('magic');
+  }
+
+  render() {
+    return (
+      <OverlayTrigger placement='bottom' delayShow={150} delayHide={5} overlay={tooltip('Домой')} >
+        <button onClick={() => this.handleClick()}>
+          <i className='fa fa-reply' aria-hidden='true' />
+        </button>
+      </OverlayTrigger>
+    );
+  }
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    setFlagsAction: bindActionCreators(setFlagsAction, dispatch),
+  }
+}
+
+const ReturnButtonMagic = connect(null, mapDispatchToProps)(withRouter(ReturnButton));
 
 const TimelineButtons = (cb) => (
   <div className='timeline__buttons'>
     <BioButton />
-    <ReturnButton />
+    <ReturnButtonMagic />
   </div>
 );
+
+
 
 export default TimelineButtons;
