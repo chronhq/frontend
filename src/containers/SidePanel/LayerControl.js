9a612073abf99d5e3df808e7999d41b7f7c3a1ca
Dmitriy Shikhaleev <padavankun@gmail.com>
2018-06-08 20:17:24 +0300

    layer control missing file

---
+++ src/containers/SidePanel/LayerControl.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class LayerControl extends React.Component {
+
+  render() {
+    return(
+      <div className='sidepanel--content'>
+        <h3> Управление слоями </h3>
+
+      </div>
+    );
+  }
+}
+
+export default LayerControl;
