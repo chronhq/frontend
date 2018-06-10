05d4c30c23531a87e78b94b29f4320617b4308b4
Dmitriy Shikhaleev <padavankun@gmail.com>
2018-06-10 13:57:15 +0300

    TraceModel remove p-things and added timedTraces data tranfomation

--- src/models/DataAdaptation/TraceModel.js
+++ src/models/DataAdaptation/TraceModel.js
@@ -4,40 +4,19 @@ export default class TraceModel {
   @observable data = {};
   @observable type = '';
 
-  @computed get projection() {
-    return this.rootStore.projection;
+  @computed get timedTraces() {
+    let fullPath = 0;
+    const dataWithTime = this.data.path[0].path.slice(1).map((n, i) => {
+      const lastPath = Math.hypot(
+        n[0] - this.data.path[0].path[i][0],
+        n[1] - this.data.path[0].path[i][1]
+      );
+      fullPath += lastPath;
+      return [...n, (fullPath / 100)];
+    });
+    dataWithTime.unshift(this.data.path[0].path[0].concat(0));
+    return [[...this.data.path[0].path[0], 0], ...dataWithTime];
   }
-
-  @computed get project() {
-    return this.projection.project;
-  }
-
-  @computed get projected() {
-    const points = [];
-    this.inTheBox.map((arr, pidx) =>
-      arr.map((flag, idx) => {
-        if (flag) {
-          const point = this.data.path[pidx].path[idx];
-          points.push(this.project([point[0], point[1]]));
-        }
-        return false;
-      }));
-    return points;
-  }
-
-  // @computed get timedTrace() {
-  //   let fullPath = 0;
-  //   // console.log('timedTrace', this.data);
-  //   console.log(this.data.path[0].path);
-  //   let dataWithTime = this.data.path[0].path;
-  //   // let dataWithTime = this.data.slice(1).map((n,i) => {
-  //     // const lastPath = Math.hypot(n[0] - data[i][0], n[1] - data[i][1]);
-  //     // fullPath = fullPath + lastPath;
-  //     // return n.concat(fullPath/100);
-  //   // });
-  //   // dataWithTime.unshift(this.data[0].concat(0));
-  //   return dataWithTime;
-  // }
 
   // Point will be in viewport area after clipping
   @computed get inTheBox() {
