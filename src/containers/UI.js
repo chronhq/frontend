a29898129d1a0232c049fc58ae6fe5b0e0c82477
Michael Orlov <MiklerGM@gmail.com>
2018-07-14 15:07:21 +0200

    Add new SVG overlay over deck.gl

--- src/containers/UI.js
+++ src/containers/UI.js
@@ -6,8 +6,9 @@ import SidePanel from '../containers/SidePanel';
 import TimePanel from '../components/TimePanel/TimePanel';
 import MapControls from '../components/MapControls';
 import Timeline from '../components/Timeline';
-import LegendHOC from '../containers/LegendHOC';
+// import LegendHOC from '../containers/LegendHOC';
 import Bio from '../components/Timeline/Bio';
+import Widgets from '../containers/Widgets';
 
 import Feedback from '../components/Feedback';
 import Intro from '../components/Intro/Carousel';
@@ -23,6 +24,7 @@ const Overlays = () => (
 
 const UI = () => (
   <div className='content'>
+    <Widgets />
     <Flag
       name='runtime.SelectedCourse'
       render={() => <Bio />}
@@ -48,12 +50,6 @@ const UI = () => (
       name="UI.MapViewport"
       render={() => <MapViewport />}
     />
-  {/*
-    <Flag
-      name="UI.LegendHOC"
-      render={() => <LegendHOC />}
-    />
-  */}
   </div>
 );
 
