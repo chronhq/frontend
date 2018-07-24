ce8384da1e56876acf8f0cec0232ae94c5b9732f
Michael Orlov <MiklerGM@gmail.com>
2018-07-25 00:22:27 +0200

    Show UI when loading

--- src/ShowCourse.js
+++ src/ShowCourse.js
@@ -2,19 +2,21 @@ import React from 'react';
 import { observer } from 'mobx-react';
 import LoadingScreen from './containers/LoadingScreen';
 import UI from './containers/UI';
-import Flag from './Flag';
+// import Flag from './Flag';
 import './ShowCourse.less';
 
 @observer
 class ShowCourse extends React.Component {
   render() {
+    console.log(this.props);
     return (
-      <Flag
-        name="runtime.CourseSelection"
-        render={props => <LoadingScreen {...props} />}
-        courseSelected={this.props.computedMatch.params.id}
-        fallbackRender={() => <UI />}
-      />
+      <div>
+        <LoadingScreen
+          {...this.props}
+          courseSelected={this.props.computedMatch.params.id}
+        />
+        <UI />
+      </div>
     );
   }
 }
