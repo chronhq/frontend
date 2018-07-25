e11dea78cb790b39c2e022f31ccd2c42f3aa3b1c
Dmitriy Shikhaleev <padavankun@gmail.com>
2018-07-26 02:34:55 +0300

    fixed an issue where seekbar show wrong date on some resize actions

--- src/components/TimePanel/SeekBar.js
+++ src/components/TimePanel/SeekBar.js
@@ -1,21 +1,54 @@
 import { observer, inject } from 'mobx-react';
 import { computed, observable } from 'mobx';
 import React from 'react';
-import PropTypes from 'prop-types';
 
 import { select, mouse } from 'd3-selection';
 import { axisBottom } from 'd3-axis';
 import { scaleLinear } from 'd3-scale';
 
+class Axis extends React.Component {
+  componentDidMount() {
+    this.renderAxis();
+  }
+
+  componentDidUpdate() {
+    this.renderAxis();
+  }
+
+  renderAxis() {
+    const axis = axisBottom()
+      .scale(this.props.scale)
+      .tickPadding([6])
+      .tickSize(10)
+      .ticks(parseInt(this.props.width / 45, 10), 'f');
+
+    select(this.svgAxis).call(axis);
+  }
+
+  render() {
+    return (
+      <g
+        className="axisTime"
+        strokeWidth="1"
+        ref={(r) => { this.svgAxis = r; }}
+      />
+    );
+  }
+}
+
 @inject('store')
 @observer
 class SeekBar extends React.Component {
+  @observable position = 0;
+
+  @observable isDown = false;
+
   componentDidMount() {
     const svg = select(this.svgTime);
     svg.on('mouseup', () => {
       const rectId = this.svgTime.childNodes[0];
       const mouseX = mouse(rectId)[0];
-      if (mouseX > 0 && mouseX < this.props.width) {
+      if (mouseX > 0 && mouseX < this.width) {
         this.now = Math.round(this.scale.invert(mouseX));
         // this.updateClockPosition();
         this.props.store.year.setYear(Number(this.now));
@@ -26,14 +59,23 @@ class SeekBar extends React.Component {
     //   select('.triangle').attr('fill', '#2f2f2f');
     //   // this.followMouse();
     });
-    this.renderBar();
   }
 
-  componentWillReceiveProps(nextProps) {
-    if (nextProps.width != this.props.width) {
-      this.renderBar();
-    }
+  @computed get width() {
+    return this.props.store.view.width < 600
+      ? this.props.store.view.width
+      : this.props.store.view.width - 300;
   }
+
+  @computed get scale() {
+    return scaleLinear()
+      .domain([this.props.store.year.min, this.props.store.year.max])
+      .range([0, this.width]);
+  }
+
+  @observable now;
+  // this is functions used to drag seeker circle and update info on the fly
+  // disabled for performance reasons
 
   // followMouse() {
   //   select('.svgTime')
@@ -64,62 +106,37 @@ class SeekBar extends React.Component {
   // this.setState({ position: this.scale(now) });
   // }
 
-  @computed get scale() {
-    return scaleLinear()
-      .domain([this.props.store.year.min, this.props.store.year.max])
-      // .range([0, 300]);
-      .range([0, this.props.width]);
-  }
-
-  // resize() {
-  //   console.log('trigger resize');
-  //   // const svgAxis =
-  //   select(this.svgAxis).call(axisBottom(this.scale).ticks(parseInt(this.width / 45, 10), 'f'));
-  //   // this.updateClockPosition();
-  //   this.forceUpdate();
-  // }
-
-  @observable position = 0;
-
-  @observable isDown = false;
-
-  @observable now;
-
-  renderBar() {
-    const axis = axisBottom(this.scale).ticks(parseInt(this.props.width / 45, 10), 'f');
-    select(this.svgAxis).call(axis);
-  }
-
 
   render() {
-    const viewBox = `-15 -25 ${this.props.width + 30} 50`;
+    const viewBox = `-15 -15 ${this.width + 30} 50`;
     return (
       <div className='timepanel__slider'>
         <svg
           className="svgTime"
           ref={(r) => { this.svgTime = r; }}
-          // width='100%'
-          width={this.props.width}
-          // height='55px'
+          width={this.width}
           viewBox={viewBox}
           preserveAspectRatio="xMaxYMin meet"
-          // preserveAspectRatio="none"
         >
-          <g className="axisTime" strokeWidth="1" ref={(r) => { this.svgAxis = r; }} />
-          <g transform={`translate(${this.scale(this.props.store.year.now)}, 0)`}>
-            <circle cx='0' r={4} style={{ fill: '#02364C', stroke: 'white', strokeWidth: 1.5 }} />
-          </g>
-          <rect x='0' y='-25' width={this.props.width + 50} height='40' fill='#ffffff' opacity='0' className='back' style={{ zIndex: -1 }} />
-          {/*
-          */}
+          <Axis width={this.width} scale={this.scale} />
+          <circle
+            cx='0'
+            r={4}
+            style={{ fill: '#02364C', stroke: 'white', strokeWidth: 1.5 }}
+            transform={`translate(${this.scale(this.props.store.year.now)}, 0)`}
+          />
+          <rect
+            x='0'
+            y='-10'
+            width={this.width + 50}
+            height='30'
+            opacity='0'
+            style={{ zIndex: -1 }}
+          />
         </svg>
       </div>
     );
   }
 }
-
-SeekBar.propTypes = {
-  width: PropTypes.number.isRequired,
-};
 
 export default SeekBar;
--- src/components/TimePanel/TimePanel.js
+++ src/components/TimePanel/TimePanel.js
@@ -1,6 +1,5 @@
 import React from 'react';
 
-import { computed } from 'mobx';
 import { inject, observer } from 'mobx-react';
 
 import ControlButtons from './ControlButtons';
@@ -10,20 +9,17 @@ import './TimePanel.less';
 @inject('store')
 @observer
 class TimePanel extends React.Component {
-  @computed get width() {
-    return this.props.store.view.width < 600
-      ? this.props.store.view.width
-      : this.props.store.view.width - 300;
-  }
-
   render() {
     return (
       <div className='timepanel'>
-        <SeekBar width={this.width} />
+        <SeekBar />
         <ControlButtons />
-        <button className='timepanel__feedbutton' onClick={() => console.log('stuff')}>
-          <i className='lnr lnr-chevron-up' aria-hidden='true' title='Open Panel' />
-        </button>
+        {/*
+          // #TODO button for expand mobile UI
+          <button className='timepanel__feedbutton' onClick={() => console.log('stuff')}>
+            <i className='lnr lnr-chevron-up' aria-hidden='true' title='Open Panel' />
+          </button>
+        */}
       </div>);
   }
 }
