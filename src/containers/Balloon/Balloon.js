6642b512d01b337470c7e48f6f8ae3cea6b1f053
Michael Orlov <MiklerGM@gmail.com>
2019-02-25 20:59:22 +0100

    split balloon component

--- src/containers/Balloon/Balloon.js
+++ src/containers/Balloon/Balloon.js
@@ -20,46 +20,26 @@ import React from 'react';
 import { inject, observer } from 'mobx-react';
 import { computed, observable, action } from 'mobx';
 
-
-import {
-  Sources,
-  PersonFact,
-  Battle,
-  Document
-} from '../../components/Messages';
-import CountryHover from '../../components/Messages/CountryHover';
-
 import './Balloon.less';
 import BalloonControls from './BalloonControls';
+import BalloonContent from './BalloonContent';
 
 @inject('store')
 @observer
 class Balloon extends React.Component {
   @observable positionClassNameCache = '';
 
-  @computed get i18n() {
-    return this.props.store.i18n.messages;
-  }
-
   @computed get balloon() {
     return this.props.store.balloon;
   }
 
-  @computed get pin() {
+  @computed get data() {
     return this.balloon.selected;
   }
 
-  @computed get pinned() {
-    return this.balloon.pinned;
-  }
-
-  @computed get countryHover() {
-    return this.balloon.countryHover;
-  }
-
   @computed get opacity() {
-    if (this.countryHover === false) {
-      return (this.pin === null || this.pin === undefined || this.pin.info.length === 0)
+    if (this.balloon.countryHover === false) {
+      return (this.data === null || this.data === undefined || this.data.info.length === 0)
         ? 0 : 1;
     }
     return 1;
@@ -75,7 +55,7 @@ class Balloon extends React.Component {
   }
 
   @computed get positionClassName() {
-    if (this.pinned) return this.positionClassNameCache;
+    if (this.balloon.pinned) return this.positionClassNameCache;
     if (this.balloon.pageX > 0.5 * window.innerWidth) {
       if (this.balloon.pageY < 0.7 * window.innerHeight) {
         return 'balloonTopRight';
@@ -87,76 +67,16 @@ class Balloon extends React.Component {
     return 'balloonBottomLeft';
   }
 
-  @computed get news() {
-    if (this.pin === null || this.pin === undefined) return [];
-    return this.pin.info.map((pin) => {
-      switch (pin.type) {
-        case 'countryHover':
-          return {
-            id: 'countryHoverBaloon',
-            message: <CountryHover id={this.pin.info[0].data} />,
-            sources: <Sources id={this.pin.info[0].data} type={this.pin.info[0].type} />
-          };
-        case 'death':
-        case 'birth':
-        // actor from wikidata
-          return {
-            id: `personHoverBalloon_${pin.person.id}`,
-            message: (
-              <PersonFact
-                person={pin.person}
-                key={`balloon_person_${pin.type}_${pin.person.id}`}
-              />
-            ),
-            sources: <Sources id={pin.person.id} pin={pin.person} type={pin.type} />
-          };
-        case 'battle':
-          return {
-            id: `battleHoverBalloon_${pin.battle.id}`,
-            message: (
-              <Battle
-                fact={pin.battle}
-                key={`balloon_battle_${pin.type}_${pin.battle.id}`}
-              />
-            ),
-            sources: <Sources id={pin.battle.id} pin={pin.battle} type={pin.type} />
-          };
-        case 'document':
-          return {
-            id: `documentHoverBalloon_${pin.document.id}`,
-            message: (
-              <Document
-                fact={pin.document}
-                key={`balloon_document_${pin.type}_${pin.document.id}`}
-              />
-            ),
-            sources: <Sources id={pin.document.id} pin={pin.document} type={pin.type} />
-          };
-        default:
-          return { message: () => '', sources: () => '', id: 'defaultBalloonId' };
-      }
-    });
-  }
-
   @action cachePosition() {
     this.positionClassNameCache = this.positionClassName;
   }
 
   render() {
     const container = `balloonContainer ${this.positionClassName}`;
-    const news = 'balloonNews balloonMain';
-    const sources = 'balloonNews balloonSources';
     return (
       <div style={{ ...this.style }} className={container}>
         <BalloonControls cachePosition={() => this.cachePosition()} />
-        <div className='balloonNewsContainer'>
-          {this.news.map(n => (
-            <div key={`news_${n.id}`} className='balloonNewsRow'>
-              <div className={news}>{n.message}</div>
-              {this.pinned && (<div className={sources}>{n.sources}</div>)}
-            </div>
-          ))}
-        </div>
+        <BalloonContent />
       </div>
     );
   }
---
+++ src/containers/Balloon/BalloonContent.js
@@ -0,0 +1,114 @@
+/*
+ * Chron.
+ * Copyright (c) 2019 Alisa Belyaeva, Ata Ali Kilicli, Amaury Martiny,
+ * Daniil Mordasov, Liam O’Flynn, Mikhail Orlov.
+ * -----
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * -----
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * -----
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+import React from 'react';
+import { inject, observer } from 'mobx-react';
+import { computed } from 'mobx';
+
+import {
+  Sources,
+  PersonFact,
+  Battle,
+  Document
+} from '../../components/Messages';
+import CountryHover from '../../components/Messages/CountryHover';
+
+
+@inject('store')
+@observer
+class BalloonContent extends React.Component {
+  @computed get i18n() {
+    return this.props.store.i18n.messages;
+  }
+
+  @computed get balloon() {
+    return this.props.store.balloon;
+  }
+
+  @computed get data() {
+    return this.balloon.selected;
+  }
+
+  @computed get news() {
+    if (this.data === null || this.data === undefined) return [];
+    return this.data.info.map((pin) => {
+      switch (pin.type) {
+        case 'countryHover':
+          return {
+            id: 'countryHoverBaloon',
+            message: <CountryHover id={this.data.info[0].data} />,
+            sources: <Sources id={this.data.info[0].data} type={this.data.info[0].type} />
+          };
+        case 'death':
+        case 'birth':
+        // actor from wikidata
+          return {
+            id: `personHoverBalloon_${pin.person.id}`,
+            message: (
+              <PersonFact
+                person={pin.person}
+                key={`balloon_person_${pin.type}_${pin.person.id}`}
+              />
+            ),
+            sources: <Sources id={pin.person.id} type={pin.type} />
+          };
+        case 'battle':
+          return {
+            id: `battleHoverBalloon_${pin.battle.id}`,
+            message: (
+              <Battle
+                fact={pin.battle}
+                key={`balloon_battle_${pin.type}_${pin.battle.id}`}
+              />
+            ),
+            sources: <Sources id={pin.battle.id} type={pin.type} />
+          };
+        case 'document':
+          return {
+            id: `documentHoverBalloon_${pin.document.id}`,
+            message: (
+              <Document
+                fact={pin.document}
+                key={`balloon_document_${pin.type}_${pin.document.id}`}
+              />
+            ),
+            sources: <Sources id={pin.document.id} type={pin.type} />
+          };
+        default:
+          return { message: () => '', sources: () => '', id: 'defaultBalloonId' };
+      }
+    });
+  }
+
+  render() {
+    const news = 'balloonNews balloonMain';
+    const sources = 'balloonNews balloonSources';
+    return (
+      <div className='balloonNewsContainer'>
+        {this.news.map(n => (
+          <div key={`news_${n.id}`} className='balloonNewsRow'>
+            <div className={news}>{n.message}</div>
+            {this.balloon.pinned && (<div className={sources}>{n.sources}</div>)}
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default BalloonContent;
