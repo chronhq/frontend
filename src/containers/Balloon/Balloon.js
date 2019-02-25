6d8bf5ec43e8bda595b99169d30c0861a9748b7b
Michael Orlov <MiklerGM@gmail.com>
2019-02-25 20:39:17 +0100

    Fixed scrollbar position for balloon #36

--- src/containers/Balloon/Balloon.js
+++ src/containers/Balloon/Balloon.js
@@ -19,7 +19,7 @@
 import React from 'react';
 import { inject, observer } from 'mobx-react';
 import { computed, observable, action } from 'mobx';
-import Button, { BUTTON_TYPE } from '../../components/Button/Button';
+
 
 import {
   Sources,
@@ -30,26 +30,31 @@ import {
 import CountryHover from '../../components/Messages/CountryHover';
 
 import './Balloon.less';
+import BalloonControls from './BalloonControls';
 
 @inject('store')
 @observer
 class Balloon extends React.Component {
   @observable positionClassNameCache = '';
 
-  @observable mouseX = 0;
+  @computed get i18n() {
+    return this.props.store.i18n.messages;
+  }
 
-  @observable mouseY = 0;
+  @computed get balloon() {
+    return this.props.store.balloon;
+  }
 
   @computed get pin() {
-    return this.props.store.balloon.selected;
+    return this.balloon.selected;
   }
 
   @computed get pinned() {
-    return this.props.store.balloon.pinned;
+    return this.balloon.pinned;
   }
 
   @computed get countryHover() {
-    return this.props.store.balloon.countryHover;
+    return this.balloon.countryHover;
   }
 
   @computed get opacity() {
@@ -60,31 +65,26 @@ class Balloon extends React.Component {
     return 1;
   }
 
-
   @computed get style() {
     return {
       opacity: this.opacity,
       visibility: this.opacity ? 'visible' : 'hidden',
-      top: `${this.props.store.balloon.pageY}px`,
-      left: `${this.props.store.balloon.pageX}px`,
+      top: `${this.balloon.pageY}px`,
+      left: `${this.balloon.pageX}px`,
     };
   }
 
   @computed get positionClassName() {
-    if (this.props.store.balloon.pinned) return this.positionClassNameCache;
-    if (this.props.store.balloon.pageX > 0.5 * window.innerWidth) {
-      if (this.props.store.balloon.pageY < 0.7 * window.innerHeight) {
+    if (this.pinned) return this.positionClassNameCache;
+    if (this.balloon.pageX > 0.5 * window.innerWidth) {
+      if (this.balloon.pageY < 0.7 * window.innerHeight) {
         return 'balloonTopRight';
       }
       return 'balloonBottomRight';
-    } if (this.props.store.balloon.pageY < 0.7 * window.innerHeight) {
+    } if (this.balloon.pageY < 0.7 * window.innerHeight) {
       return 'balloonTopLeft';
     }
     return 'balloonBottomLeft';
-  }
-
-  @computed get i18n() {
-    return this.props.store.i18n.messages;
   }
 
   @computed get news() {
@@ -138,64 +138,24 @@ class Balloon extends React.Component {
     });
   }
 
-  @action setMousePosition(e) {
-    this.mouseX = e.pageX;
-    this.mouseY = e.pageY;
-  }
-
-  mouseDragDown = (e) => {
+  @action cachePosition() {
     this.positionClassNameCache = this.positionClassName;
-    this.setMousePosition(e);
-    this.props.store.balloon.dragClick(true);
-  }
-
-  mouseDrag = (e) => {
-    e.preventDefault();
-    if (this.props.store.balloon.dragOn) {
-      this.props.store.balloon.setPosition(
-        this.props.store.balloon.pageX - (this.mouseX - e.pageX),
-        this.props.store.balloon.pageY - (this.mouseY - e.pageY)
-      );
-      this.setMousePosition(e);
-    }
   }
 
   render() {
     const container = `balloonContainer ${this.positionClassName}`;
     const news = 'balloonNews balloonMain';
     const sources = 'balloonNews balloonSources';
-    /* eslint-disable jsx-a11y/no-static-element-interactions */
     return (
       <div style={{ ...this.style }} className={container}>
-        {this.props.store.balloon.pinned
-          && (
-            <div className='balloonControls'>
-              <div
-                className='balloonDrag'
-                onMouseDown={this.mouseDragDown}
-                onMouseUp={() => this.props.store.balloon.dragClick(false)}
-                onMouseLeave={() => this.props.store.balloon.dragClick(false)}
-                onMouseMove={this.mouseDrag}
-              >
-                <hr />
-              </div>
-              <Button
-                btnType={BUTTON_TYPE.CLOSE}
-                onClick={() => this.props.store.balloon.unpin()}
-              >
-                <span className="lnr lnr-cross" />
-              </Button>
-            </div>
-          )}
+        <BalloonControls cachePosition={() => this.cachePosition()} />
         <div className='balloonNewsContainer'>
-          <div className={news}>
-            {this.news.map(n => (<div key={`news_${n.id}`}>{n.message}</div>))}
-          </div>
-          {this.pinned && (
-            <div className={sources}>
-              {this.news.map(n => (<div key={`sources_${n.id}`}>{n.sources}</div>))}
+          {this.news.map(n => (
+            <div key={`news_${n.id}`} className='balloonNewsRow'>
+              <div className={news}>{n.message}</div>
+              {this.pinned && (<div className={sources}>{n.sources}</div>)}
             </div>
-          )}
+          ))}
         </div>
       </div>
     );
---
+++ src/containers/Balloon/BalloonControls.js
@@ -0,0 +1,79 @@
+/*
+ * Chron.
+ * Copyright (c) 2019 Alisa Belyaeva, Ata Ali Kilicli, Amaury Martiny,
+ * Daniil Mordasov, Liam O’Flynn, Mikhail Orlov.
+ * -----
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * -----
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * -----
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+import React from 'react';
+import { inject, observer } from 'mobx-react';
+import { observable, action } from 'mobx';
+import Button, { BUTTON_TYPE } from '../../components/Button/Button';
+
+
+@inject('store')
+@observer
+class BalloonControls extends React.Component {
+  @observable mouseX = 0;
+
+  @observable mouseY = 0;
+
+  @action setMousePosition(e) {
+    this.mouseX = e.pageX;
+    this.mouseY = e.pageY;
+  }
+
+  mouseDragDown = (e) => {
+    this.props.cachePosition();
+    this.setMousePosition(e);
+    this.props.store.balloon.dragClick(true);
+  }
+
+  mouseDrag = (e) => {
+    e.preventDefault();
+    if (this.props.store.balloon.dragOn) {
+      this.props.store.balloon.setPosition(
+        this.props.store.balloon.pageX - (this.mouseX - e.pageX),
+        this.props.store.balloon.pageY - (this.mouseY - e.pageY)
+      );
+      this.setMousePosition(e);
+    }
+  }
+
+  render() {
+    if (!this.props.store.balloon.pinned) return '';
+    /* eslint-disable jsx-a11y/no-static-element-interactions */
+    return (
+      <div className='balloonControls'>
+        <div
+          className='balloonDrag'
+          onMouseDown={this.mouseDragDown}
+          onMouseUp={() => this.props.store.balloon.dragClick(false)}
+          onMouseLeave={() => this.props.store.balloon.dragClick(false)}
+          onMouseMove={this.mouseDrag}
+        >
+          <hr />
+        </div>
+        <Button
+          btnType={BUTTON_TYPE.CLOSE}
+          onClick={() => this.props.store.balloon.unpin()}
+        >
+          <span className="lnr lnr-cross" />
+        </Button>
+      </div>
+    );
+  }
+}
+
+export default BalloonControls;
