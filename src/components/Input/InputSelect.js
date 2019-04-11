5bb1bff048880cba0ee747e3d8887d5e3207cf2c
Michael Orlov <MiklerGM@gmail.com>
2019-04-12 00:06:45 +0200

    [WIP] Fix InputSelect component

--- src/components/Input/InputSelect.js
+++ src/components/Input/InputSelect.js
@@ -22,26 +22,35 @@ import PropTypes from 'prop-types';
 
 import './InputSelect.less';
 
+const downButton = () => (
+  <svg version="1.1" width="15" height="15" viewBox="0 0 20 20">
+    <path fill="#000000" d="M0 6c0-0.128 0.049-0.256 0.146-0.354 0.195-0.195 0.512-0.195 0.707 0l8.646 8.646 8.646-8.646c0.195-0.195 0.512-0.195 0.707 0s0.195 0.512 0 0.707l-9 9c-0.195 0.195-0.512 0.195-0.707 0l-9-9c-0.098-0.098-0.146-0.226-0.146-0.354z" />
+  </svg>
+);
+
 class InputSelect extends Component {
   static defaultProps = {
-    value: ''
+    value: '',
+    style: {},
   }
 
   render() {
     return (
       <Select
         // disabled={this.state.disabled}
-        style={{ width: '220px' }}
+        style={this.props.style}
         onSelect={v => this.props.cb(v)}
         notFoundContent={false}
         dropdownMenuStyle={{ maxHeight: 400 }}
         placeholder={this.props.placeholder}
         value={this.props.value}
+        optionLabelProp='children'
+        inputIcon={downButton()}
         backfill
       >
-        {Object.keys(this.props.options).map(key => (
-          <Option value={this.props.options[key]} key={key} text={this.props.options[key]}>
-            {this.props.options[key]}
+        {this.props.options.map(o => (
+          <Option value={o.key} key={`is-${o.key}`}>
+            {o.value}
           </Option>
         ))
         }
@@ -51,6 +60,7 @@ class InputSelect extends Component {
 }
 
 InputSelect.propTypes = {
+  style: PropTypes.object,
   value: PropTypes.any, // It should be a string
   placeholder: PropTypes.string.isRequired,
   options: PropTypes.array.isRequired,
--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -16,6 +16,8 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
+import 'rc-select/assets/index.css';
+
 import InputCheckBox from './InputCheckBox';
 import InputNumber from './InputNumber';
 import InputRange from './Slider';
