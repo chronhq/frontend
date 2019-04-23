4e35cd03f7bde07bdf4f970e0c02b0560a5d70cb
Michael Orlov <MiklerGM@gmail.com>
2019-04-23 20:35:55 +0200

    Remove EADH demo page

--- src/routes.js
+++ src/routes.js
@@ -30,7 +30,6 @@ const Narrative = lazy(() => import('./pages/Narrative'));
 const NotFound = lazy(() => import('./pages/404'));
 const BadGateway = lazy(() => import('./pages/502'));
 const GatewayTimeout = lazy(() => import('./pages/504'));
-const Demo = lazy(() => import('./pages/Demo'));
 const About = lazy(() => import('./pages/About'));
 
 const AppRouter = () => (
@@ -42,7 +41,6 @@ const AppRouter = () => (
       <Route path='/504' render={() => <GatewayTimeout />} />
       <Route path='/502' render={() => <BadGateway />} />
       <Route path='/404' render={() => <NotFound />} />
-      <Route path='/demo' render={() => <Demo />} />
       <Route path='/about' render={() => <About />} />
       <Route path='/:id' render={({ match }) => <Narrative story={match.params.id} />} />
     </Switch>
