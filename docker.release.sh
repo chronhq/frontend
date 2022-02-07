324f86ca83ee52b51c3badfd5f81500b5aa2511e
Michael Orlov <MiklerGM@gmail.com>
2022-02-07 22:42:44 +0100

    Docker Image with configs via ENV

--- docker.release.sh
+++ docker.release.sh
@@ -2,20 +2,18 @@
 
 ACTION=$1
 TAG=$2
-EXT=$3
 
 if [ "${TAG}" = "" ]; then TAG="dev"; fi
-if [ "${EXT}" = "" ]; then EXT=${TAG}; fi
 
 if [[ "${ACTION}" = "build" || "${ACTION}" = "buildpush" || "${ACTION}" = "push" ]]; then
-  echo "Action: '${ACTION}'; Tag: '${TAG}'; CFG_EXT: ${EXT}"
+  echo "Action: '${ACTION}'; Tag: '${TAG}';"
 else
   echo $0 '<action> <docker-tag> ?<config-postfix>'
   echo $0 '<build|push|buildpush>' '<dev|prod>' '?<dev|prod>'
 fi
 
 if [[ "${ACTION}" = "build" || "${ACTION}" = "buildpush" ]]; then
-  docker build -t "chronmaps/frontend:release-${TAG}" --target release --build-arg CFG_EXT=${EXT} .
+  docker build -t "chronmaps/frontend:release-${TAG}" --target release .
 fi
 if [[ "${ACTION}" = "push" || "${ACTION}" = "buildpush" ]]; then
   docker push "chronmaps/frontend:release-${TAG}"
